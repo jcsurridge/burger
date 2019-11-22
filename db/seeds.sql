5754fd0d7af72193409b00ab9f6ddf3ea01aa917
unknown <josh.c.surridge@gmail.com>
2019-11-21 22:28:49 -0700

    added connection, server, and .sql files + db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Hamburger', false);
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger', false);
